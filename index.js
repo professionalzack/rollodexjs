755e04b595a4d6c18371d854ed6e05a3e35f0304
zack <zack.etc@gmail.com>
2018-08-02 14:08:33 -0700

    Procfile

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 client.commands = new Discord.Collection();
 const commandFiles = fs.readdirSync('./controllers').filter(file => file.endsWith('.js'));
 const { prefix } = process.env.prefix || require('./config.json');
-const { token } = process.env.token || require('./config.json');
+const { token } = process.env.BOT_TOKEN || require('./config.json');
 
 var db = 'mongodb://localhost/rollodexjs'
 
