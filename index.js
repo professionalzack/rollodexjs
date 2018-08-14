1d0d266e062ffc892d11b98437fc558ddd891884
zack <zack.etc@gmail.com>
2018-08-13 18:31:15 -0700

    roll test cleanup

--- index.js
+++ index.js
@@ -69,11 +69,10 @@ client.on('message', message => {
             var i;
             var result = parseInt(bonus) || 0;
             var rolls = (amt || 1);
-            message.channel.send("amount: " + amt + ", rolls: " + rolls)
             for (i = 0; i < rolls; i++) {
                 let once = face(size)
                 let rollnumber = i + 1;
-                message.channel.send("roll " + rollnumber + ": " + once + " ( " + i);
+                message.channel.send("roll " + rollnumber + ": " + once);
                 result = result + once;
             };
             return result
